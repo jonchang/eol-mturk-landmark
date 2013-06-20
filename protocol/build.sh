9d9ceae3808b7eb2da2f1e6bc2cdf8fe89b98ffd
Jonathan Chang <jchang641@gmail.com>
2013-06-19 18:23:27 -0700

    Don't build self-contained help.

--- protocol/build.sh
+++ protocol/build.sh
@@ -2,7 +2,7 @@
 
 MDFILE=protocol
 
-(pandoc -S $MDFILE.markdown --self-contained --toc -t html5 --mathml -c buttondown.min.css -o $MDFILE.html) &
+(pandoc -S $MDFILE.markdown --toc -t html5 -c buttondown.min.css -o $MDFILE.html) &
 
 (pandoc -S $MDFILE.markdown -o $MDFILE.pdf -V geometry:'margin=1in'
 gs -o ${MDFILE}small.pdf -sDEVICE=pdfwrite -f $MDFILE.pdf
