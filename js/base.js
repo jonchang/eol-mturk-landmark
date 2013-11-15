753d0f131f16d4cd1354438fab73c7e50c871228
Jonathan Chang <jchang641@gmail.com>
2013-11-15 15:41:14 -0800

    Handle linear measurements and semilandmarks.

--- js/base.js
+++ js/base.js
@@ -1,112 +1,230 @@
-/*jslint browser: true, vars: true, passfail: true*/
+/*jslint browser: true, vars: true*/
 /*global $, jQuery*/
 
 
-if (!String.prototype.supplant) {
-    String.prototype.supplant = function (o) {
-        return this.replace(
-            /\{([^{}]*)\}/g,
-            function (a, b) {
-                var r = o[b];
-                return typeof r === 'string' || typeof r === 'number' ? r : a;
-            }
-        );
-    };
-}
-
-var cats = {
-    "M1": 'Mouth',
-    "M2": 'Mouth',
-    "M3": 'Mouth',
-    "E1": 'Eye',
-    "P1": 'Pectoral fin',
-    "P2": 'Pectoral fin',
-    "D1": 'Dorsal fin',
-    "D2": 'Dorsal fin',
-    "A1": 'Anal fin',
-    "A2": 'Anal fin',
-    "C1": 'Caudal fin',
-    "C2": 'Caudal fin',
-    "C3": 'Caudal fin',
-    "C4": 'Caudal fin'
+var tool_defs = {
+    "M1": {
+        kind: "point",
+        help: "The front upper tip of the mouth opening."
+    },
+    "M2": {
+        kind: "point",
+        help: "The back of the mouth, showing the angle of the mouth opening."
+    },
+    "M3": {
+        kind: "point",
+        help: "The front lower tip of the mouth opening."
+    },
+    "C1": {
+        kind: "point",
+        help: "Where the top of the caudal fin meets the body."
+    },
+    "C2": {
+        kind: "point",
+        help: "Where the bottom of the caudal fin meets the body."
+    },
+    "P1": {
+        kind: "point",
+        help: "Where the top of the pectoral fin meets the body."
+    },
+    "P2": {
+        kind: "point",
+        help: "Where the bottom of the pectoral fin meets the body."
+    },
+    "O1": {
+        kind: "point",
+        help: "The top point of the gill cover opening."
+    },
+    "O2": {
+        kind: "point",
+        help: "The point of the gill cover opening closest to the tail of the fish."
+    },
+    "O3": {
+        kind: "point",
+        help: "The bottom point of the gill cover opening."
+    },
+    "E1": {
+        kind: "point",
+        help: "A point on the eye closest to the head of the fish."
+    },
+    "E2": {
+        kind: "point",
+        help: "A point on the eye closest to the tail of the fish."
+    },
+    "SF": {
+        kind: "curve",
+        help: "The curve of the body from the tip of the mouth to the start of the dorsal fin (the fish's \"forehead\")."
+    },
+    "SD": {
+        kind: "curve",
+        help: "The curve of the body along the dorsal fin."
+    },
+    "SA": {
+        kind: "curve",
+        help: "The curve of the body along the anal fin."
+    },
+    "SC": {
+        kind: "curve",
+        help: "The curve of the body from the tip of the mouth to the start of the ventral fin (the fish's \"chin\")."
+    },
+    "LL": {
+        kind: "line",
+        help: "The horizontal line from the tip of the mouth to the beginning of the caudal fin (between points C1 and C2)."
+    },
+    "LD": {
+        kind: "line",
+        help: "The vertical line along the tallest (i.e., deepest) part of the fish, excluding fins."
+    }
 };
-
-
-
-var helps = {
-    "M1": 'Mark the front upper tip of the mouth opening. (shortcut: m)',
-    "M2": 'Mark the back of the mouth, showing the angle of the opening. (shortcut: m)',
-    "M3": 'Mark the front lower tip of the mouth opening. (shortcut: m)',
-    "E1": 'Mark the center of the eye. (shortcut: e)',
-    "P1": 'Mark the top of the pectoral fin, where the fin ray attaches to the fleshy body of the fish. (shortcut: t)',
-    "P2": 'Mark the bottom of the pectoral fin, where the fin ray attaches to the fleshy body of the fish. (shortcut: t)',
-    "D1": 'Mark the front of the rearmost dorsal fin, where the first fin ray attaches to the fleshy body of the fish. (shortcut: d)',
-    "D2": 'Mark the back of the rearmost dorsal fin, where the last fin ray attaches to the fleshy body of the fish. (shortcut: d)',
-    "A1": 'Mark the front of the anal fin, where the first fin ray attaches to the fleshy body of the fish. (shortcut: a)',
-    "A2": 'Mark the back of the anal fin, where the last fin ray attaches to the fleshy body of the fish. (shortcut: a)',
-    "C1": 'Mark the top of the caudal fin, where the fleshy part of the caudal fin attaches to the body of the fish. This is usually the narrowest part of the fish. (shortcut: c)',
-    "C2": 'Mark the spot where the top of the fin ray attaches to the fleshy part of the caudal fin. There is usually a change in the color or texture of the fin occurs. (shortcut: c)',
-    "C3": 'Mark the spot where the bottom of the fin ray attaches to the fleshy part of the caudal fin. There is usually a change in the color or texture of the fin occurs. (shortcut: c)',
-    "C4": 'Mark the bottom of the caudal fin, where the fleshy part of the caudal fin attaches to the body of the fish. This tends to be the narrowest part of the fish. (shortcut: c)'
-};
-
-var helpimgs = {
-    "Mouth": "img/mouth.jpg",
-    "Eye": "img/eye.jpg",
-    "Dorsal fin": "img/dorsal.jpg",
-    "Anal fin": "img/anal.jpg",
-    "Pectoral fin": "img/pectoral.jpg",
-    "Caudal fin": "img/caudal.jpg"
-}
 
 function create_image(src) {
     "use strict";
     $("#canvasbg").attr("src", src);
 }
 
+var tool_types = (function () {
+    "use strict";
+    function draw_point(args) {
+        var ctx = args.ctx;
+        var radius = 6;
+        ctx.beginPath();
+        ctx.arc(args.x, args.y, radius, 0, 2 * Math.PI);
+        ctx.fillStyle = "white";
+        ctx.fill();
+
+        ctx.strokeStyle = "#666";
+        ctx.stroke();
+
+        ctx.fillStyle = "black";
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.font = "5pt sans-serif";
+        ctx.fillText(args.tool, args.x, args.y);
+        update_data(args.tool, [[args.x, args.y]]);
+    }
+
+    function draw_line(args) {
+        var ctx = args.ctx;
+        if (args.evt === "mousedown") {
+            ctx.startx = args.x;
+            ctx.starty = args.y;
+        }
+        ctx.beginPath();
+        ctx.moveTo(ctx.startx, ctx.starty);
+        ctx.lineTo(args.x, args.y);
+        ctx.closePath();
+        ctx.strokeStyle = "red";
+        ctx.stroke();
+        draw_point({x: ctx.startx, y: ctx.starty, tool: args.tool, ctx: ctx});
+        draw_point({x: args.x, y: args.y, tool: args.tool, ctx: ctx});
+        update_data(args.tool, [[ctx.startx, ctx.starty], [args.x, args.y]]);
+    }
+
+    function draw_curve(args) {
+        var ctx = args.ctx;
+        function rect_at(pp) {
+            ctx.fillRect(pp[0]-1,pp[1]-1,3,3);
+        }
+        function dist(p1, p2) {
+            return Math.sqrt(Math.pow(p1[0] - p2[0], 2) + Math.pow(p1[1] - p2[1], 2));
+        }
+        if (args.evt === "mousedown") {
+            ctx.startx = args.x;
+            ctx.starty = args.y;
+            ctx.endx = undefined;
+            ctx.endy = undefined;
+            ctx.pts = [];
+        } else if (args.evt === "mousemove") {
+            ctx.pts.push([args.x, args.y]);
+            ctx.lineTo(args.x, args.y);
+        } else if (args.evt === "mouseup") {
+            ctx.endx = args.x;
+            ctx.endy = args.y;
+        }
+        ctx.beginPath();
+        ctx.strokeStyle = "red";
+        ctx.moveTo(ctx.startx, ctx.starty);
+        for (var ii = 0; ii < ctx.pts.length; ii++) {
+            ctx.lineTo(ctx.pts[ii][0], ctx.pts[ii][1]);
+        }
+        ctx.strokeStyle = "red";
+        ctx.stroke();
+        draw_point({x: ctx.startx, y: ctx.starty, tool: args.tool, ctx: ctx});
+        if (ctx.endx && ctx.endy) {
+            // Find the arclength of the canvas curve, approximated by line segments
+            var total_dist = 0;
+            for (var jj = 0; jj < ctx.pts.length - 1; jj++) {
+                total_dist += dist(ctx.pts[jj], ctx.pts[jj+1]);
+            }
+
+            // We want 5 semilandmarks, so that splits the arc into 4 subsegments
+            var step = total_dist / 4;
+
+            var running_dist = 0;
+            var running_step = step;
+            var semilandmarks = [];
+            for (var jj = 0; jj < ctx.pts.length - 1; jj++) {
+                var curdist = dist(ctx.pts[jj], ctx.pts[jj+1]);
+                running_dist += curdist;
+                if (running_dist > running_step) {
+                    var frac = running_step / running_dist;
+                    var newx = (1 - frac) * ctx.pts[jj][0] + frac * ctx.pts[jj+1][0];
+                    var newy = (1 - frac) * ctx.pts[jj][1] + frac * ctx.pts[jj+1][1];
+                    semilandmarks.push([newx, newy]);
+                    ctx.fillStyle = "yellow";
+                    rect_at([newx, newy]);
+                    running_step += step;
+                }
+            }
+            // add in the start and end points
+            semilandmarks.unshift([ctx.startx, ctx.starty]);
+            semilandmarks.push([ctx.endx, ctx.endy]);
+            draw_point({x: ctx.endx, y: ctx.endy, tool: args.tool, ctx: ctx});
+            update_data(args.tool, semilandmarks);
+        }
+    }
+
+    function clear(fn) {
+        return function (args) {
+            args.ctx.clearRect(0, 0, args.ctx.canvas.width, args.ctx.canvas.height);
+            fn(args);
+        };
+    }
+
+
+    return {
+        point: clear(draw_point),
+        line: clear(draw_line),
+        curve: clear(draw_curve)
+    };
+}());
+
 function create_buttons() {
     "use strict";
     var toolbox = $("#toolbox"),
-        canvasbox = $("#canvasbox")[0],
+        canvasbox = cbox[0],
         canvas_template = $("#appcanvas")[0];
-    for (var key in cats) {
-        if (cats.hasOwnProperty(key)) {
-            toolbox.append('<label class="btn btn-default tool" id="{key}" data-toggle="tooltip"><input type="radio" name="tools">{key}</label>'.supplant({key: key}));
+    for (var key in tool_defs) {
+        if (tool_defs.hasOwnProperty(key)) {
+            toolbox.append('<label class="btn btn-default tool" id="' + key +
+                           '" data-toggle="tooltip"><input type="radio" name="tools">' + key + '</label>');
             // make canvases
             var canvas = canvas_template.cloneNode(false);
             canvas.id = key + "_canvas";
             canvasbox.appendChild(canvas);
             canvas.getContext("2d").globalAlpha = 0.85;
-
         }
     }
     var tools = $(".tool");
     tools.tooltip();
     tools.click(function() {
-        $("#active-tool").attr("value", $(this).attr("id"))
-        update_help(get_active_tool());
+        active_tool($(this).attr("id")); // set this as the current tool
     });
     tools[0].click();
-
 }
 
-function update_help(name) {
-    var infobox = $("#infobox-content")[0];
-    infobox.innerHTML = "";
-    if (name in cats && cats[name] in helpimgs) {
-        var img = document.createElement("img");
-        img.src = helpimgs[cats[name]];
-        img.className = "help";
-        infobox.appendChild(img);
-    }
-    if (name in helps) {
-        var pp = document.createElement("p");
-        pp.textContent = "(" + name + ") " + helps[name];
-        infobox.appendChild(pp);
-    }
-}
-
-function get_active_tool () {
+function active_tool(set_to) {
+    if (set_to) $("#active-tool").attr("value", set_to);
     return $("#active-tool").attr("value");
 }
 
@@ -116,70 +234,6 @@ function update_data(name, value) {
     landmark_data[name] = value;
     $("#form-marks")[0].value = JSON.stringify(landmark_data);
     update_submit();
-    draw_lines();
-}
-
-var neighbors = [
-    [["M1", "M2"], ["M2", "M3"]],
-    [["P1", "P2"]],
-    [["D1", "D2"]],
-    [["A1", "A2"]],
-    [["C1", "C2"], ["C2", "C3"], ["C3", "C4"]],
-];
-
-function draw_line(p1, p2, ctx) {
-    ctx.beginPath();
-    ctx.moveTo(p1[0], p1[1]);
-    ctx.lineTo(p2[0], p2[1]);
-    ctx.closePath();
-    ctx.strokeStyle = "red";
-    ctx.stroke();
-}
-
-function draw_lines() {
-    var canvas = $("#appcanvas")[0];
-    var ctx = canvas.getContext("2d");
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    neighbors.map(function (arr) {
-        for (var ii = 0; ii < arr.length; ii++) {
-            if (arr[ii][0] in landmark_data && arr[ii][1] in landmark_data) {
-                draw_line(landmark_data[arr[ii][0]], landmark_data[arr[ii][1]], ctx);
-            }
-        }
-    });
-}
-
-function landmark (evt) {
-    var appbox = $("#appbox")[0];
-    var x = evt.pageX - appbox.offsetLeft;
-    var y = evt.pageY - appbox.offsetTop;
-    draw_landmark(x, y, get_active_tool());
-}
-
-
-function draw_landmark(x, y, tool) {
-    clearSelection();
-    var canvas = $("#" + tool + "_canvas");
-    if (!canvas.length) return;
-
-    var radius = 7;
-    var ctx = canvas[0].getContext("2d");
-    ctx.clearRect(0, 0, canvas[0].width, canvas[0].height);
-
-    ctx.beginPath();
-    ctx.arc(x, y, radius, 0, 2 * Math.PI);
-    ctx.fillStyle = "white";
-    ctx.fill();
-
-    ctx.strokeStyle = "#666";
-    ctx.stroke();
-
-    ctx.fillStyle = "black";
-    ctx.textAlign = "center";
-    ctx.textBaseline = "middle";
-    ctx.font = "6pt sans-serif";
-    ctx.fillText(tool, x, y);
-    update_data(tool, [x, y])
 }
 
 // from somewhere on stack overflow
@@ -195,25 +249,13 @@ function clearSelection() {
     }
 }
 
-function next_tool() {
-    var tool = get_active_tool();
-
-}
-
 var shortcuts = {
-    "m": ["M1" ,"M2", "M3"],
-    "e": ["E1"],
-    "d": ["D1", "D2"],
-    "a": ["A1", "A2"],
-    "t": ["P1", "P2"],
-    "c": ["C1", "C2", "C3", "C4"],
-    "f": Object.keys(cats)
 }
 
 function evt_keydown(evt) {
     var key = String.fromCharCode(evt.keyCode || evt.which);
     if (key in shortcuts) {
-        var idx = shortcuts[key].indexOf(get_active_tool())
+        var idx = shortcuts[key].indexOf(active_tool())
         if (idx >= 0) {
             var newtool = shortcuts[key][idx + 1] || shortcuts[key][0];
         } else {
@@ -221,15 +263,6 @@ function evt_keydown(evt) {
         }
         $("#" + newtool).click();
     }
-}
-
-function evt_mdown (evt) {
-    landmark(evt);
-    $("#canvasbox").on("mousemove", landmark);
-}
-
-function evt_mup (evt) {
-    $("#canvasbox").off("mousemove", landmark);
 }
 
 function evt_submit (evt) {
@@ -258,7 +291,7 @@ function review_on(txt) {
 
 function update_submit () {
     var have = Object.keys(landmark_data).length | 0;
-    var want = Object.keys(cats).length | 0;
+    var want = Object.keys(tool_defs).length | 0;
     var submit = $("#submitButton")[0];
     if (have < want) {
         submit.disabled = true;
@@ -273,11 +306,35 @@ function update_submit () {
     }
 }
 
+function get_canvas(tool) {
+    return $("#" + tool + "_canvas")[0].getContext("2d");
+}
+
+function get_evt_xy (evt) {
+    var appbox = $("#appbox")[0];
+    var x = evt.pageX - appbox.offsetLeft;
+    var y = evt.pageY - appbox.offsetTop;
+    return [x, y];
+}
+
+function evt_mouse(e) {
+    if (e.type == "mousedown") cbox.on("mousemove", evt_mouse);
+    if (e.type == "mouseup") cbox.off("mousemove", evt_mouse);
+    var tool = active_tool();
+    var appbox = $("#appbox")[0];
+    var args = {x: e.pageX - appbox.offsetLeft,
+                y: e.pageY - appbox.offsetTop,
+                evt: e.type,
+                tool: tool,
+                ctx: $("#" + tool + "_canvas")[0].getContext("2d")
+               };
+    tool_types[tool_defs[tool].kind](args);
+}
+
 // initialization
 $(document).ready(function() {
-    var canvasbox = $("#canvasbox");
-    canvasbox.on("mousedown", evt_mdown);
-    canvasbox.on("mouseup", evt_mup);
+    cbox = $("#canvasbox"); // global canvasbox
+    cbox.on("mousedown mouseup", evt_mouse);
     $(document).keypress(evt_keydown);
     $("#mturk_form").submit(evt_submit);
     create_buttons();
