b21ded4e92a521a015a8f3605493f108aebfb87a
Jonathan Chang <jchang641@gmail.com>
2016-02-05 19:18:49 -0800

    Fix bad warning

--- js/check.js
+++ js/check.js
@@ -10,8 +10,8 @@ function check(obj) {
     if ((obj.C2 && obj.C2[1]) > (obj.C3 && obj.C3[1])) acc.push("C3 is above C2, are they switched around?");
     if ((obj.C5 && obj.C5[1]) > (obj.C4 && obj.C4[1])) acc.push("C4 is above C5, are they switched around?");
     if ((obj.C1 && obj.C1[1]) > (obj.C5 && obj.C5[1])) acc.push("C5 is above C1, are they switched around?");
-    if ((obj.C3 && obj.C3[1]) > (obj.C6 && obj.C6[1])) acc.push("C3 is above C6, are they switched around?");
-    if ((obj.C6 && obj.C6[1]) > (obj.C2 && obj.C2[1])) acc.push("C6 is above C2, are they switched around?");
+    if ((obj.C3 && obj.C3[1]) < (obj.C6 && obj.C6[1])) acc.push("C3 is above C6, are they switched around?");
+    if ((obj.C6 && obj.C6[1]) < (obj.C2 && obj.C2[1])) acc.push("C6 is above C2, are they switched around?");
     if ((obj.O5 && obj.O5[1]) < (obj.O4 && obj.O4[1])) acc.push("O5 is above O4, are they switched around?");
     if ((obj.O2 && obj.O2[1]) < (obj.O1 && obj.O1[1])) acc.push("O2 is above O1, are they switched around?");
     if (obj.V1 && obj.J1 && obj.C5) {
